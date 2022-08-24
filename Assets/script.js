d27938d23039b3640ebf10c1d592e963b926665e
Bambi <leighasteele10@gmail.com>
2022-08-24 10:48:05 -0400

    added answer variable

--- Assets/script.js
+++ Assets/script.js
@@ -5,26 +5,32 @@ var quizEl = document.querySelector("#quiz");
 var endEl = document.querySelector("#end");
 var startButton = document.querySelector("#start-button");
 var submitScoreButton = document.querySelector("#submit-score");
-var quizTitle = document.querySelector("#quiz#title");
+var quizTitle = document.querySelector("#title");
 var questionsEl = document.querySelector("#questions");
 
 var questions = [
-    {number: "One", possibleAnswers: [1,2,3,4], correct: 0 },
-    {number: "Two", possibleAnswers: [5,6,7,8], correct: 3 },
-    {number: "Three", possibleAnswers: [9,10,11,12], correct: 2 },
-    {number: "Four", possibleAnswers: [13,14,15,16], correct: 1 },
+    {question: "One", possibleAnswers: [1,2,3,4], correct: 0 },
+    {question: "Two", possibleAnswers: [5,6,7,8], correct: 3 },
+    {question: "Three", possibleAnswers: [9,10,11,12], correct: 2 },
+    {question: "Four", possibleAnswers: [13,14,15,16], correct: 1 },
 ];
 var cursor = 0;
 
 function displayState() {
     if (state === 'start') {
         startEl.style.display = 'block';
+        quizEl.style.display = 'none';
+        endEl.style.display = 'none';
     }
     if (state === 'quiz') {
+        startEl.style.display = 'none';
         quizEl.style.display = 'block';
+        endEl.style.display = 'none';
         displayQuestion();
     }
     if (state === 'end') {
+        startEl.style.display = 'none';
+        quizEl.style.display = 'none';
         endEl.style.display = 'block';
     }
 }
@@ -40,14 +46,15 @@ function reset() {
 }
 
 startButton.addEventListener("click", function(){
+    state = 'quiz';
     displayQuestion();
     displayState();
 });
 
 
-quizTitle.addEventListener("click", function (event) {
+questionsEl.addEventListener("click", function (event) {
 var element = event.target;
-if (element.matches('h2')) {
+if (element.matches('')) {
     var index = Array.from(element.parentElement.children).indexOf(element);
     console.log(index);
     cursor++;
@@ -63,9 +70,14 @@ if (element.matches('h2')) {
 
 
 function displayQuestion() {
-    state = 'quiz';
-    var titleText = questions [cursor]; 
+
+    var titleText = questions[cursor].question; 
     quizTitle.textContent = titleText;
+
+    var firstAnswer = questions[cursor].possibleAnswers[0];
+    var firstAnswerBtn = document.createElement("button");
+    firstAnswerBtn.textContent = firstAnswer;
+    questionsEl.append(firstAnswerBtn);
 }
 
 submitScoreButton.addEventListener("click", function(){
